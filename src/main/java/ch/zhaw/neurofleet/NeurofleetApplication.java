95efc9b8481ac1cefd50d3eb5946c0b3a7a2c2d0
ISxOdin <ilias.strub@gmail.com>
2025-02-22 13:49:26 +0100

    first commit

---
+++ src/main/java/ch/zhaw/neurofleet/NeurofleetApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.neurofleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurofleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurofleetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/neurofleet/NeurofleetApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.neurofleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeurofleetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
