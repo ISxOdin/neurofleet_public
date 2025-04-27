c85c946a2d0946926fa9d7f246b0395ad3276913
ISxOdin <ilias.strub@gmail.com>
2025-04-27 16:25:47 +0200

    Implemented company entity closes #34 and API endpoints closes #35, closes #36, closes #37, closes #38, closes #39

---
+++ src/main/java/ch/zhaw/neurofleet/controller/CompanyController.java
@@ -0,0 +1,66 @@
+package ch.zhaw.neurofleet.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.zhaw.neurofleet.model.Company;
+import ch.zhaw.neurofleet.model.CompanyCreateDTO;
+import ch.zhaw.neurofleet.repository.CompanyRepository;
+
+@RestController
+@RequestMapping("/api")
+public class CompanyController {
+    @Autowired
+    CompanyRepository companyRepository;
+
+
+    @PostMapping("/companies")
+    public ResponseEntity<Company> createCompany(
+            @RequestBody CompanyCreateDTO cDTO) {
+        Company cDAO = new Company(cDTO.getName(), cDTO.getEmail(), cDTO.getAddress());
+        Company c = companyRepository.save(cDAO);
+        return new ResponseEntity<>(c, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/companies")
+    public ResponseEntity<Page<Company>> getCompanies(
+            @RequestParam(required = false, defaultValue = "1") Integer pageNumber,
+            @RequestParam(required = false, defaultValue = "5") Integer pageSize) {
+        Page<Company> allCompanies = companyRepository.findAll(PageRequest.of(pageNumber - 1, pageSize));
+        return new ResponseEntity<>(allCompanies, HttpStatus.OK);
+    }
+
+    @GetMapping("/companies/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable String id) {
+        Optional<Company> c = companyRepository.findById(id);
+        if (c.isPresent()) {
+            return new ResponseEntity<>(c.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/companies/{id}")
+    public ResponseEntity<String> deleteCompanyById(@PathVariable String id) {
+        if (companyRepository.existsById(id)) {
+            companyRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("DELETED");
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
